8e15b6ad78bcfe9d7e6063294cd4caf4fb5ab8ad
Elliot Voris <elliot@stellar.org>
2023-07-13 12:21:19 -0500

    Adding a `createCreateAccountTransaction` function

--- src/lib/stellar/transactions.js
+++ src/lib/stellar/transactions.js
@@ -4,13 +4,46 @@ const horizonUrl = 'https://horizon-testnet.stellar.org'
 const networkPassphrase = Networks.TESTNET
 
 /**
- * Constructs and returns a Stellar transaction that contains a payment operaion(s) and an optional memo.
+ * Constructs and returns a Stellar transaction that contains a `createAccount` operation and an optional memo.
  * @param {Object} opts Options object
  * @param {string} opts.source Public Stellar address to use as the source account of the transaction
- * @param {string|string[]} opts.destination Public Stellar address(es) to receive the payment
- * @param {string} [opts.asset=native] Asset to be sent to the destination address(es) (example: USDC:GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5)
+ * @param {string} opts.destination Public Stellar address to be created on the network
+ * @param {number|string} opts.amount Amount to be sent as the destination account's starting balance
+ * @param {string} [opts.memo] Memo to add to the transaction
+ */
+export async function createCreateAccountTransaction({ source, destination, amount, memo }) {
+    let server = new Server(horizonUrl)
+    let sourceAccount = await server.loadAccount(source)
+    let transaction = new TransactionBuilder(sourceAccount, {
+        networkPassphrase: networkPassphrase,
+        fee: '100000',
+    })
+
+    if (memo) {
+        transaction.addMemo(Memo.text(memo))
+    }
+
+    transaction.addOperation(Operation.createAccount({
+        destination: destination,
+        startingBalance: amount.toString(),
+    }))
+
+    let builtTransaction = transaction.setTimeout(300).build()
+    console.log('$src/stellar/transactions.js createCreateAccountTransaction builtTransaction', builtTransaction)
+    return {
+        transaction: builtTransaction.toXDR(),
+        network_passphrase: networkPassphrase,
+    }
+}
+
+/**
+ * Constructs and returns a Stellar transaction that contains a `payment` operaion and an optional memo.
+ * @param {Object} opts Options object
+ * @param {string} opts.source Public Stellar address to use as the source account of the transaction
+ * @param {string} opts.destination Public Stellar address to receive the payment
+ * @param {string} [opts.asset=native] Asset to be sent to the destination address (example: USDC:GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5)
  * @param {number|string} opts.amount Amount of the asset to send in the payment
- * @param {string} [opts.memo] Memo to add onto the transaction
+ * @param {string} [opts.memo] Memo to add to the transaction
  * @returns {Promise<{
  *     transaction: string,
  *     network_passphrase: string,
@@ -33,21 +66,11 @@ export async function createPaymentTransaction({ source, destination, asset, amo
         transaction.addMemo(Memo.text(memo))
     }
 
-    if (Array.isArray(destination)) {
-        destination.map((dest) => {
-            transaction.addOperation(Operation.payment({
-                destination: dest,
-                amount: amount.toString(),
-                asset: sendAsset,
-            }))
-        })
-    } else {
-        transaction.addOperation(Operation.payment({
-            destination: destination,
-            amount: amount.toString(),
-            asset: sendAsset,
-        }))
-    }
+    transaction.addOperation(Operation.payment({
+        destination: destination,
+        amount: amount.toString(),
+        asset: sendAsset,
+    }))
 
     let builtTransaction = transaction.setTimeout(300).build()
     console.log('$src/stellar/transactions.js createPaymentTransaction builtTransaction', builtTransaction)
