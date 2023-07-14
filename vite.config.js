ae60a8215a0ef506153b7a0c5eb4d1c1acf015e4
Elliot Voris <elliot@stellar.org>
2023-07-13 20:08:46 -0500

    injecting window.js during the build process

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import inject from '@rollup/plugin-inject'
+import path from 'path'
 
 export default defineConfig({
     plugins: [sveltekit()],
@@ -16,4 +18,13 @@ export default defineConfig({
             ],
         },
     },
+    build: {
+        rollupOptions: {
+            plugins: [
+                inject({
+                    window: path.resolve('src/lib/window.js')
+                })
+            ]
+        }
+    }
 })
