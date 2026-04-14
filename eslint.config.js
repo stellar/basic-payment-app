14d7b11f64c230178e26a726f1ea2d7b94610337
Elliot Voris <elliot@stellar.org>
2026-04-14 12:35:46 -0500

    revert to static site generation, upgrade eslint

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import prettier from 'eslint-config-prettier'
+import path from 'node:path'
+import { includeIgnoreFile } from '@eslint/compat'
+import js from '@eslint/js'
+import svelte from 'eslint-plugin-svelte'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import svelteConfig from './svelte.config.js'
+
+const gitignorePath = path.resolve(import.meta.dirname, '.gitignore')
+
+export default defineConfig([
+    includeIgnoreFile(gitignorePath),
+    js.configs.recommended,
+    svelte.configs.recommended,
+    prettier,
+    svelte.configs.prettier,
+    {
+        languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    },
+
+    {
+        files: ['**/*.svelte', '**/*.svelte.js'],
+        languageOptions: { parserOptions: { svelteConfig } },
+    },
+
+    {
+        // Override or add rule settings here, such as:
+        // 'svelte/button-has-type': 'error'
+        rules: {},
+    },
+])
